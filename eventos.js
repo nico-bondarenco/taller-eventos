fb4bd75ebd3f2a7ffb0c7bcfcb1fab9fca5c6368
nico-bondarenco <nicobondarenco@gmail.com>
2023-08-19 12:31:00 -0300

    Se agregó el div

---
+++ eventos.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const divertido = document.getElementById("contenedoreishon")
+    divertido.addEventListener("click", () => alert('Hola! Soy el div'))
+})
