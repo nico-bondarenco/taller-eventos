fce58441df64ef63f7a6329a629aea516049e483
nico-bondarenco <nicobondarenco@gmail.com>
2023-08-19 13:21:35 -0300

    Solución burbujeo

--- eventos.js
+++ eventos.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const divertido = document.getElementById("contenedoreishon")
+    const divertido = document.getElementById("contenedoreishon");
+    const boton = document.getElementById("boton");
+
+    boton.addEventListener("click", (e) => {
+        e.stopPropagation();
+        alert("Hola!");
+    })
     divertido.addEventListener("click", () => alert('Hola! Soy el div'))
+
+
 })
